55643fc8c9217dfff146291c14a09f6cafe37063
Jose Humberto Vasquez <darksoul_1012@hotmail.es>
2017-11-29 12:17:59 -0600

    banner de bienvenida

--- UnicaesGestion/Controllers/UserController.cs
+++ UnicaesGestion/Controllers/UserController.cs
@@ -13,5 +13,10 @@ namespace UnicaesGestion.Controllers
         {
             return View();
         }
+
+        public ActionResult Banner()
+        {
+            return View();
+        }
     }
 }
