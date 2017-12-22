c5734f10fe5f95a3a8e684e929ff27e56ba3cb6a
Jose Humberto Vasquez <darksoul_1012@hotmail.es>
2017-12-22 16:57:58 -0600

    js para obtener los datos que estan en la ventana modal.  Funcion para tablas de una columna.

---
+++ UnicaesGestion/Content/js/ModalFunction.js
@@ -0,0 +1,29 @@
+﻿//var Enviar = function () {
+
+    
+//}
+
+function Obtener() {
+    var txtNp = jQuery('#npaso').val();
+    var cbxR = jQuery('#cbxResponsable').val();
+    var txta = jQuery('#descripcion').val();
+    var cbxF = jQuery('#figura').val();
+    var slctd = $('select#my_multi_select1').val();
+
+    var dependences = [];
+    $.each(slctd, function (key,value) {       
+        dependences.push($('#'+value+'-selection > span').text());
+    });
+
+
+    var result = {
+        'paso': txtNp,
+        'responsable': cbxR,
+        'descripcion': txta,
+        'figura': cbxF,
+        'dependencias':dependences
+    };
+
+ 
+
+} 
